8d3b7748f7db613dde0be6c30247ae619f47c712
chuckie_chen <chuckie_chen@foxmail.com>
2017-12-12 23:45:27 +0800

    Add and init hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: chuckie_chen
+	> Mail: chuckie_chen@foxmail.com
+	> Created Time: 2017年12月12日 星期二 23时43分41秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main(int argc,char *argv[])
+{
+    printf("Hello world!\n");
+
+    return 0;
+}
